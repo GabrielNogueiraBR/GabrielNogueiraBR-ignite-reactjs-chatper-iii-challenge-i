58db9edba430848a1a45a88ae8a153ac840d8e22
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-08-12 09:59:31 -0300

    chore: creating Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={commonStyles.container}>
+        <img src="/assets/logo.svg" alt="logo" className={styles.logo} />
+      </div>
+    </header>
+  );
 }
