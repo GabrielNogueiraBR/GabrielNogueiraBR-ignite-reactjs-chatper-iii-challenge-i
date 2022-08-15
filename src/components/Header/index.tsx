974ea858bcc087582830e181e5f82f03ce94ae79
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-08-15 09:48:12 -0300

    chore: adding link in logo icon on Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './header.module.scss';
 
@@ -5,7 +6,9 @@ export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
       <div className={commonStyles.container}>
-        <img src="/assets/logo.svg" alt="logo" className={styles.logo} />
+        <Link href="/">
+          <img src="/assets/logo.svg" alt="logo" className={styles.logo} />
+        </Link>
       </div>
     </header>
   );
